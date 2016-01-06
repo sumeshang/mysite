771dcdc15751fdc47825f0943e8c9b8c6327b0c5
ss <sumesh.kk@rapidvaluesolution.com>
2016-01-06 13:53:54 +0530

    bug fixed

--- polls/models.py
+++ polls/models.py
@@ -23,5 +23,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):              # __unicode__ on Python 2
-    # return self.choice_text
+        return self.choice_text
